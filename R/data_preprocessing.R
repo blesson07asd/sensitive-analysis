10131decdefdde54fd236153c2de9ad90841253c
Blesson <blessonthomas2005@gmail.com>
2025-08-09 02:31:31 +0530

    Update data_preprocessing.R

--- R/data_preprocessing.R
+++ R/data_preprocessing.R
@@ -7,7 +7,8 @@ data <- read.csv("DailyDelhiClimateTest.csv", stringsAsFactors = FALSE)
 
 data_clean<-data %>% drop_na() #removing rows with missing values
 
-write.csv(data_clean,"data_clean.csv",row.names = FALSE) # Save the cleaned data
+write.csv(data_clean,"data_clean.csv",row.names = FALSE) #Save the cleaned data
 head(data_clean)
+
 
 
